86393ff21a12fdab3f44d984082c9d10752038c0
sagarelnino <shuvashishpaul64@gmail.com>
2021-09-10 18:11:48 +0600

    fixed

--- push.js
+++ push.js
@@ -9,7 +9,7 @@ console.log(vapidKeys);
 
 push.setVapidDetails('mailto:shuvashishpaul64@gmail.com',vapidKeys.publicKey, vapidKeys.privateKey);
 
-let sub = {};
+let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/d3-OVPNcGIw:APA91bGOzJgCO8Q4IliDDAx6Q8PtB1e_TQrplOpldJ8vyGO7UTP807aeHeEh3VZFp9C7ROv8Q_SynkG0VDNdGcR1YsXcDetMNbse1ESUk1NLHKa3a_uJoBWS7e7dvfK9Ven5_GRxxZyJ","expirationTime":null,"keys":{"p256dh":"BI0BMFZoeT8W54CHWpYXbJ7RqxTvgyXiX7cMM4DRDSEpOdxDysyvvuKJcQRa5P6lJ8pMU0h917m1K1no2vVPCHw","auth":"fMAW8CBYSEM8NzW3HT2dzg"}};
 
 push.sendNotification(sub,'test message');
 
