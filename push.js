82203cfa873b681fd66a93a782c17fd28f8906e1
sagarelnino <shuvashishpaul64@gmail.com>
2021-09-10 14:12:30 +0600

    done again

--- push.js
+++ push.js
@@ -8,7 +8,7 @@ let vapidKeys = {
 
 push.setVapidDetails('mailto:shuvashishpaul64@gmail.com',vapidKeys.publicKey, vapidKeys.privateKey);
 
-let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/f9dyuzP2eeY:APA91bG-A0bJs-ZMxE-TgKnRCvJF3ITG3XAfF1M_vuv7kO0MTthymwF7PoVhcD4g8BOnMqESli7cjj8MMMI7FY_sXht4TToqWuFK8OcupLDHI2gwndZd2HF_pVYQ3tIgo3hde-0mhgRV","expirationTime":null,"keys":{"p256dh":"BGtUGs9P624eY5lG85KpRHYzm5JanDPOcL11fl9nz_EV6iRuh4K43f_iF_v1jQUg6w6YxpWmCUhXhO0scxdhwFk","auth":"6ZL_tIf2PaCxIO9_Ur-YyQ"}};
+let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/dkfKfONdJHE:APA91bH53aWNvjNKAhTtJxuU4WrHF0fcseNxB-GwSoTqxnRgnROtWJBO2XP4HEdA-FSMbrLPfNRaifJ1DeM_Zg86PBpJbxrVidaXPh2_v_srUn-0I9EpN2LfbpHgCTQSkYxFRgjByrx0","expirationTime":null,"keys":{"p256dh":"BECVcz72gTLAua_bPj7UcEZ2z23Wljxxg9scpPD9uHbAQpAZlgguxnusWUU_XPt4Mutatr3elfXLJRzavFo_2lw","auth":"OfFXipCIY_ntxHdxBZETzQ"}};
 
 push.sendNotification(sub,'test message');
 
