541d44da6855064467edc01df72bdd4de951ae99
sagarelnino <shuvashishpaul64@gmail.com>
2021-09-10 13:56:54 +0600

    done

--- sw.js
+++ sw.js
@@ -1,3 +1,49 @@
-self.addEventListener('push', () => {
-    self.registration.sendNotification('test message',{});
+let cacheData = "appV1";
+this.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(cacheData).then((cache) => {
+            cache.addAll([
+                "/index.html",
+            ])
+        })
+    )
 });
+
+this.addEventListener("fetch", (event) => {
+    if (!navigator.onLine) {
+        this.registration.showNotification("Offline", {
+                    body: "No internet connection.",
+    })
+        event.respondWith(
+            caches.match(event.request).then((resp) => {
+                if (resp) {
+                    return resp;
+                }
+                let requestURL = event.request.clone();
+                fetch(requestURL);
+            })
+        )
+    }
+})
+
+self.addEventListener('push', event => {
+    const options = {
+      body: 'This notification was generated from a push!',
+      icon: 'images/notification-flat.png',
+      vibrate: [100, 50, 100],
+      data: {
+        dateOfArrival: Date.now(),
+        primaryKey: 1
+      },
+      actions: [
+        {action: 'explore', title: 'Go to the site',
+          icon: 'images/checkmark.png'},
+        {action: 'close', title: 'Close the notification',
+          icon: 'images/xmark.png'},
+      ]
+    };
+  
+    event.waitUntil(
+      self.registration.showNotification('Push Notification', options)
+    );
+  });
