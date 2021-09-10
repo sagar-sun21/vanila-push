19ee4caee5224f96d311d1558ff1b83612a49258
sagarelnino <shuvashishpaul64@gmail.com>
2021-09-10 18:28:45 +0600

    torres

--- push.js
+++ push.js
@@ -4,8 +4,8 @@ let vapidKeys = {
     publicKey: 'BGLLkYqXZ560JeDiUq_Oa4Jx3nIMkX6sqN22B_O2hHc8o5AM0M9R89FyWBn4n4pijlrLoNWQFoic6Ad2HmwhK2A',
     privateKey: 'k2FnEaimd8zkjpK-ndVIjjIoHri9o7AFiIWaSjSNe5w'
   };
-let vapidKeys = push.generateVAPIDKeys();
-console.log(vapidKeys);
+// let vapidKeys = push.generateVAPIDKeys();
+// console.log(vapidKeys);
 
 push.setVapidDetails('mailto:shuvashishpaul64@gmail.com',vapidKeys.publicKey, vapidKeys.privateKey);
 
